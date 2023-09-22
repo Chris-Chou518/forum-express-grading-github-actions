00ee7744e88c2deb07208a51a2c8118f30b0de11
Chris-Chou518 <ttlyes496@gmail.com>
2023-09-22 20:23:35 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 express-handlebars
 const routes = require('./routes')
-
+// 載入controller
 const app = express()
 const port = process.env.PORT || 3000
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs

cec910c757d28d3c4401123a5f77235bb804cd66
Chris-Chou518 <ttlyes496@gmail.com>
2023-09-23 22:57:21 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,17 +2,26 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db // const User = db.User的解構
 const UserController = {
-  signUpPage: (req, res) => {
+  signUpPage: (req, res, next) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    return bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match')
+    return User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
-      .then(() => { return res.redirect('/signin') })
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = UserController
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', (req, res) => res.redirect('/restaurants')) //  影片是router.get
+router.use('/', generalErrorHandler) // 這邊為何要加'/' 以及為什麼不放在app.js
 
 module.exports = router

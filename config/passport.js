f62e0d4091013bcf50d02bea2f63e3429e1a28b8
Chris-Chou518 <ttlyes496@gmail.com>
2023-09-25 01:56:59 +0800

    change user to JSON

--- config/passport.js
+++ config/passport.js
@@ -28,6 +28,8 @@ passport.serializeUser((user, cb) => {
 })
 passport.deserializeUser((id, cb) => {
   User.findByPk(id).then(user => {
+    user = user.toJSON() // 加入這行
+    // console.log(user)
     return cb(null, user)
   })
 })

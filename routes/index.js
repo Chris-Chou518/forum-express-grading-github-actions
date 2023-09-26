e524399002c64ca79842b4d0d6aeece708b59c4d
Chris-Chou518 <ttlyes496@gmail.com>
2023-09-26 13:00:42 +0800

    feat: add restaurant model

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
-router.get('/logout', userController.logout) // 為何不用post?
+router.get('/logout', userController.logout) // 為何不用post? :都可 get會友快取，可能會導致實際上未登出
 router.get('/restaurants', authenticated, restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants')) //  影片是router.get
-router.use('/', generalErrorHandler) // 這邊為何要加'/' 以及為什麼不放在app.js
+router.use('/', (req, res) => res.redirect('/restaurants')) //  影片是router.get(只用於get請求) (use:可用於get.post.put...等請求)
+router.use('/', generalErrorHandler) // 這邊為何要加'/' 以及為什麼不放在app.js(都可)
 
 module.exports = router

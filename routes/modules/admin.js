0ca06f72bb974e1bdd670e59421352835aca65b5
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-01 13:29:52 +0800

    feat:category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -13,5 +13,6 @@ router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/users', adminController.getUsers)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
